45031cfe4ef13eb80c3a7f39dff150da4f169772
Fayez O Saadi <fayezosaadi@gmail.com>
2017-04-10 03:16:11 +0000

    code that returns a http response

--- https_example.js
+++ https_example.js
@@ -1,18 +1,19 @@
-var https = require('https');
-console.log('I did it!');
+  var https = require('https');
+  console.log('I did it!');
 
-var options = {
-  host: 'www.example.org',
-  path: '/'
-};
+  var options = {
+    host: 'www.example.org',
+    path: '/'
+  };
 
-// called by https when the request is made.
-var callback = function() {
-  console.log('In response handler callback!');
-}
+  // called by https when the request is made.
+  var callback = function(response) {
+    console.log('In response handler callback!');
+    console.log('Response: ', response);
+  }
 
-console.log("I'm about to make the request!");
+  console.log("I'm about to make the request!");
 
-https.request(options, callback).end();
+  https.request(options, callback).end();
 
-console.log("I've made the request!");
+  console.log("I've made the request!");
